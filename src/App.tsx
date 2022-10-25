1556f95fc72d0e10c9ebc94d25d08cece5a297b0
haqnawaz <haqnawaz99@yahoo.com>
2022-10-25 20:45:22 +0500

    First commit with Box added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import {Box} from "@chakra-ui/react"
+
+function App() {
+  return (
+    <div className="App">
+      <Box as='section' bg='green' w='120px' h='150px'>
+        This is a box
+      </Box>
+    </div>
+  )
+}
+
+export default App
