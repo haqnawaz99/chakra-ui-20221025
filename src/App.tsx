be4b23f36e0ff92c4cbeef609ec6012dd56d4198
haqnawaz <haqnawaz99@yahoo.com>
2022-10-25 21:07:47 +0500

    Stack Hstack and Vstack added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 
 import './App.css'
-import {Box, Flex} from "@chakra-ui/react"
+import {Box, Flex, Stack, VStack, HStack} from "@chakra-ui/react"
 
 function App() {
   return (
     <div className="App">
+      <HStack spacing='24px' direction='row'>
+        {/* <Stack spacing='24px' direction='row'> */}
+        {/* <VStack spacing='24px' direction='row'> */}
+        {/* <HStack spacing='24px' direction='row'> */}
+      <Box bg='green' w='100px' h='200px'>
+          item1
+        </Box>
+        <Box bg='blue' w='100px' h='200px'>
+          item1
+        </Box>
+        <Box bg='red' w='200px' h='100px'>
+          item1
+        </Box>
+        <Box bg='tomato' w='100px' h='200px'>
+          item1
+        </Box>
+        </HStack>
       {/* <Box as='section' bg='green' w='120px' h='150px'>
         This is a box
       </Box> */}
-      <Flex justify='space-between' align='center'>
+      {/* <Flex justify='space-between' align='ce'>
         <Box bg='green' w='100px' h='200px'>
           item1
         </Box>
@@ -21,7 +38,7 @@ function App() {
         <Box bg='tomato' w='100px' h='200px'>
           item1
         </Box>
-      </Flex>
+      </Flex> */}
     </div>
   )
 }
