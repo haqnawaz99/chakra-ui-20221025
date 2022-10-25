0dbddcd3a4f2c9b11008a2b19bfbecca484686bc
haqnawaz <haqnawaz99@yahoo.com>
2022-10-25 20:56:46 +0500

    flex added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 
 import './App.css'
-import {Box} from "@chakra-ui/react"
+import {Box, Flex} from "@chakra-ui/react"
 
 function App() {
   return (
     <div className="App">
-      <Box as='section' bg='green' w='120px' h='150px'>
+      {/* <Box as='section' bg='green' w='120px' h='150px'>
         This is a box
-      </Box>
+      </Box> */}
+      <Flex justify='space-between' align='center'>
+        <Box bg='green' w='100px' h='200px'>
+          item1
+        </Box>
+        <Box bg='blue' w='100px' h='200px'>
+          item1
+        </Box>
+        <Box bg='red' w='200px' h='100px'>
+          item1
+        </Box>
+        <Box bg='tomato' w='100px' h='200px'>
+          item1
+        </Box>
+      </Flex>
     </div>
   )
 }
